aaa8e8ee1008a9dc91536e1a9d7ba2d2fef2acff
jadehuynh <jadehuynh08@gmail.com>
2022-04-20 17:14:17 -0500

    working on functions, req, res, routes

---
+++ routes/apiroutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const data = require('../db/db.json');
+const fs = require('fs')
+const exportData = (data) => {
+    fs.writeFileSync("../db/db.json", JSON.stringify(data))
+}
+router.get("/api/notes", (req,res) => {
+    res.json(data)
+})
+
+router.post("/api/notes", (req,res) => {
+    // data.push(req.body)
+    console.log(req.body)
+    res.json("Note added.")
+    exportData();
+})
+
+// router.delete("/api/notes", (req,res) => {
+
+// })
+module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./routes/apiroutes.js');
+const noteRoutes = require('./routes/noteroute.js')
 // const htmlRoutes = require('./routes/htmlRoutes');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+
+app.use("/", apiRoutes)
+app.use("/", noteRoutes)
 app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
