cb0fecc97e2ddba997634caea72afab4abf08cc7
jadehuynh <jadehuynh08@gmail.com>
2022-04-20 22:32:04 -0500

    displayed some input on notetaker

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,19 +1,24 @@
 const router = require('express').Router();
 const data = require('../db/db.json');
 const fs = require('fs')
+const infoHolder = require('../db/db.json')
 const exportData = (data) => {
-    fs.writeFileSync("../db/db.json", JSON.stringify)
+    fs.writeFileSync("./db/db.json", JSON.stringify)
 }
 router.get("/api/notes", (req,res) => {
     res.json(data)
 })
 
 router.post("/api/notes", (req,res) => {
+    var data = req.body
+    infoHolder.push(data)
+    console.log(req.body)
     res.json("Note added.")
+    exportData(data);
 })
 // router.post("/api/notes", (req,res) => {
 //     data.push(req.body)
-//     console.log(req.body)
+
 //     res.json("Note added.")
 //     // exportData(data);
 // })
