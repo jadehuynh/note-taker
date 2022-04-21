58b2fc532b3086921bc0c5ab8183359bb05e8586
jadehuynh <jadehuynh08@gmail.com>
2022-04-21 17:57:09 -0500

    testing heroku

--- routes/noteroute.js
+++ routes/noteroute.js
@@ -1,5 +1,9 @@
+const { fstat } = require('fs');
 const path = require('path');
 const router = require('express').Router();
+// const displayNote = (data) => {
+//     fs.readFileSync("./db/db.json", JSON.parse(data))
+// }
 
 router.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
@@ -7,5 +11,8 @@ router.get("/notes", (req,res) => {
 router.post("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 })
+// router.get("/notes", (req,res) => {
+//     res.displayNote(data);
+// })
 
 module.exports = router
