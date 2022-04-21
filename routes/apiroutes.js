8967fe2fcceca5177fcf7a4a4b240faa1217e0b6
jadehuynh <jadehuynh08@gmail.com>
2022-04-21 10:06:45 -0500

    saving api

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,21 +1,26 @@
 const router = require('express').Router();
 const data = require('../db/db.json');
 const fs = require('fs')
-const infoHolder = require('../db/db.json')
+// const infoHolder = require('../db/db.json')
 const exportData = (data) => {
-    fs.writeFileSync("./db/db.json", JSON.stringify)
+    fs.writeFileSync("./db/db.json", JSON.stringify(data))
 }
 router.get("/api/notes", (req,res) => {
     res.json(data)
 })
 
 router.post("/api/notes", (req,res) => {
-    var data = req.body
-    infoHolder.push(data)
+    // var data = req.body
+    // infoHolder.push(data)
+    data.push(req.body)
     console.log(req.body)
     res.json("Note added.")
     exportData(data);
 })
+
+// app.get('*', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/public/index.html'))
+// });
 // router.post("/api/notes", (req,res) => {
 //     data.push(req.body)
 
