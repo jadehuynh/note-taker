39bfd075da27c3d31c8f9521063289fc32c77395
jadehuynh <jadehuynh08@gmail.com>
2022-05-05 21:57:33 -0700

    polished json file

--- routes/noteroute.js
+++ routes/noteroute.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
+
 const displayNote = (data) => {
     fs.readFileSync("./db/db.json", JSON.parse(data))
 }
